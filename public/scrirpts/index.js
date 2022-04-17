c4da37c23d99676fa58ab39fbb5ac26b1808f0c7
Aryex82 <ajeya0297@gmail.com>
2022-04-17 23:13:31 +0530

    pages

---
+++ public/scrirpts/index.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+  $("#sign-up-btn").click(function(){
+    $("#sign-up-modal").modal('show');
+  });
+});
+
+
+$(document).ready(function(){
+  $("#login-btn").click(function(){
+    $("#login-modal").modal('show');
+  });
+});
